31b4eac4220de72a8ceaea84b69312ff85d2b697
Rahul Pandey <hello@rkpandey.com>
2020-05-08 22:20:52 -0700

    Clear backstack when logging out

--- app/src/main/java/edu/stanford/rkpandey/instafire/ProfileActivity.kt
+++ app/src/main/java/edu/stanford/rkpandey/instafire/ProfileActivity.kt
@@ -20,7 +20,9 @@ class ProfileActivity : PostsActivity() {
         if (item.itemId == R.id.menu_logout) {
             Log.i(TAG, "User wants to logout")
             FirebaseAuth.getInstance().signOut()
-            startActivity(Intent(this, LoginActivity::class.java))
+            val logoutIntent = Intent(this, LoginActivity::class.java)
+            logoutIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(logoutIntent)
         }
         return super.onOptionsItemSelected(item)
     }
